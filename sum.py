07268eaccd8c44b61fd9bece6d2abc86f43a1b70
Tim Marro <tmarro@bu.edu>
2024-09-15 14:50:38 -0400

    changed file name

---
+++ sum.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("The sum is:", num1 + num2)
--- test.py
+++
@@ -1,4 +0,0 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-print("The sum is:", num1 + num2)
